0d273c4e4b8d9861459db8a7986e9bab74a120f9
Zuned khan <zunedkhan107@gmail.com>
2026-03-03 17:10:06 +0530

    Remove undefsafe package and related files to streamline dependencies

--- server/index.js
+++ server/index.js
@@ -11,6 +11,11 @@ app.use(express.json());
 import aiRoutes from "./routes/ai.routes.js";
 app.use('/api', aiRoutes)
 const port = process.env.PORT || 7000;
+
 app.listen(port, () => {
     console.log(`\n 🚀 Server running on http://localhost:${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Backend is Live 🚀");
 });
